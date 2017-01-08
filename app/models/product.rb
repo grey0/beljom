4386609ec7e13b081fcee2718188a2cebcdab519
Abass Sesay <bassco10@gmail.com>
2017-01-08 16:54:33 -0500

    add validation for adding more than 5 images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
   validates :name, :description, presence: true
-
+  validate :image_count
   mount_uploaders :product_images, ProductImageUploader
+
+  def image_count
+    if product_images.count > 5
+      errors.add(:product_images, 'you cannot upload more than 5 files')
+    end
+  end
 end
