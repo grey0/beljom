884459b035587ddf19b7685b7af58d37f74b8791
Abass Sesay <bassco10@gmail.com>
2017-01-15 12:58:59 -0500

    heroku assets fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,13 +23,13 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = true
+  config.assets.compile = true
 
   ########################################
-  config.cache_classes = true
-  config.serve_static_files = true
-  config.assets.compile = true
-  config.assets.digest = true
+  # config.cache_classes = true
+  # config.serve_static_files = true
+  # config.assets.compile = true
+  # config.assets.digest = true
   ######################################## STOVFL - bootstrap heroku
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
